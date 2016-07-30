aedf18a47371b447f767372ab191fcafc5725855
coreychen71 <coreychen71@gmail.com>
2016-07-30 22:27:29 +0800

    2016/07/30 1.新增每日16:30寄出待修設備清單 2.更改判斷時間方式

--- EverydaySPnlCountService/MainMethod.cs
+++ EverydaySPnlCountService/MainMethod.cs
@@ -6,18 +6,20 @@ using System.Data;
 using System.Text;
 using System.Timers;
 using EWPCB_SPnlCountClass;
+using NPOI.SS.UserModel;
+using NPOI.HSSF.UserModel;
 
 namespace EverydaySPnlCountService
 {
     class MainMethod
     {
         private Timer timer;
-        private DateTime setTime;
+        //private DateTime setTime;
         private DateTime nowTime;
         //設定間隔時間為1分鐘
         private double timerInterval = 1 * 60 * 1000;
         private string datetimeFormat = "yyyy-MM-dd HH:mm:ss";
-        private string SaveFile = Path.GetTempPath() + "SPnlCount.txt";
+        private string SaveFile = Path.GetTempPath();
         private string LogPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + 
             "\\EveryDaySPnlCountLog.txt";
         private StreamWriter writerResult;
@@ -37,26 +39,41 @@ namespace EverydaySPnlCountService
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
             timer.Stop();
-            if(CheckTime())
+            //每日06:30進行SPnl數統計
+            if (CheckTime("06:30","06:31"))
             {
                 SPnlCountRun();
-            }            
+            }
+            //每日16:00進行待修設備清單
+            if (CheckTime("16:00","16:01"))
+            {
+                EquipMaintainRun();
+            }
             timer.Start();
         }
 
-        private bool CheckTime()
+        private bool CheckTime(string time1,string time2)
         {
             bool result = false;
-            setTime = DateTime.Parse("06:30");
             nowTime = DateTime.Now;
+            //##### 2016/07/30 改用判斷是否在指定的時間區段內 #####//
+            DateTime t1 = DateTime.Parse(time1);
+            DateTime t2 = DateTime.Parse(time2);
+            /* ##### 原先一開始所使用的秒數差距判斷 #####
+            setTime = DateTime.Parse(settime);
             double interval = nowTime.Subtract(setTime).TotalSeconds;
-            //差距在45秒內均為true
-            if (interval >= -45 && interval <= 45)
+            //差距在60秒內均為true
+            if (interval >= -60 && interval <= 60)
             {
                 result = true;
                 writerLog.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "   CheckTime true_" + nowTime +
                     "_interval=" + interval);
                 writerLog.Flush();
+            }
+            */
+            if (nowTime >= t1 && nowTime <= t2)
+            {
+                result = true;
             }
             else
             {
@@ -72,8 +89,9 @@ namespace EverydaySPnlCountService
         {
             try
             {
+                SaveFile += "SPnlCount.txt";
                 //### 2016/07/21 依總經理指示，再將起始日往前推1個月。 ###//
-                DateTime startDate = nowTime.AddMonths(-1);
+                DateTime startDate = DateTime.Now.AddMonths(-1);
                 DataTable result = SPnlCount.SPnlCountRun(startDate);
                 //此方式為每次寫入時，均會直接覆蓋掉原本內容
                 writerResult = new StreamWriter(SaveFile);
@@ -87,7 +105,10 @@ namespace EverydaySPnlCountService
                 writerResult.Close();
                 writerLog.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "   Insert Temp OK!");
                 writerLog.Flush();
-                SendMail();
+                SendMail("sm4@ewpcb.com.tw", "每日預估入庫SPnl數量統計", "spnlcount@ewpcb.com.tw",
+                    DateTime.Now.ToString("yyyy-MM-dd") + " 每日預估入庫SPnl數量統計！",
+                    "每日預估入庫SPnl數量統計，請詳閱附件。" + "<br/>" + "<br/>" +
+                    "-----此封郵件由系統所寄出，請勿直接回覆！-----", SaveFile);
             }
             catch (Exception ex)
             {
@@ -96,14 +117,107 @@ namespace EverydaySPnlCountService
             }
         }
 
-        private void SendMail()
+
+        private void EquipMaintainRun()
+        {
+            try
+            {
+                SaveFile += "EquipMaintain.xls";
+                DataTable[] result = new DataTable[] { EquipMaintain.GetWaitMaintain() };
+                string[] strSheet = new string[] { "每日待修設備清單" };
+                DataTableToExcel(result, strSheet, SaveFile);
+                writerLog.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "   Insert Temp OK!");
+                writerLog.Flush();
+                SendMail("sm4@ewpcb.com.tw", "每日待修設備清單", "equip@ewpcb.com.tw",
+                    DateTime.Now.ToString("yyyy-MM-dd") + " 每日待修設備清單！",
+                    "每日待修設備清單，請詳閱附件。" + "<br/>" + "<br/>" +
+                    "-----此封郵件由系統所寄出，請勿直接回覆！-----", SaveFile);
+            }
+            catch (Exception ex)
+            {
+                writerLog.WriteLine(DateTime.Now.ToString(datetimeFormat) + "  " + ex.Message + "\r\n");
+                writerLog.Flush();
+            }
+        }
+
+        /// <summary>
+        /// 將資料表轉成Eecel檔案
+        /// 若轉出並存檔成功，傳回true
+        /// </summary>
+        /// <param name="Source">要轉出的資料表陣列</param>
+        /// <param name="SheetName">工作表名陣列</param>
+        /// <param name="SavePath">存檔路徑及檔名</param>
+        /// <param name="ExcelRevi">要存檔的Excel版本，2007以上為true</param>
+        private bool DataTableToExcel(DataTable[] Source, string[] SheetName, string SavePath)
+        {
+            bool result = false;
+            IWorkbook workbook;
+            ISheet Sheet;
+            workbook = new HSSFWorkbook();
+            HSSFCellStyle csTitle = (HSSFCellStyle)workbook.CreateCellStyle();
+            HSSFCellStyle csCell = (HSSFCellStyle)workbook.CreateCellStyle();
+            HSSFFont fontTitle = (HSSFFont)workbook.CreateFont();
+            HSSFFont fontCell = (HSSFFont)workbook.CreateFont();
+            fontTitle.FontName = "微軟正黑體";
+            fontTitle.FontHeightInPoints = 11;
+            fontTitle.Boldweight = (short)FontBoldWeight.Bold;
+            fontCell.FontName = "微軟正黑體";
+            fontCell.FontHeightInPoints = 11;
+            csTitle.SetFont(fontTitle);
+            csCell.SetFont(fontCell);
+            for (int AllSheet = 0; AllSheet < Source.Length; AllSheet++)
+            {
+                if (Source[AllSheet].TableName != string.Empty)
+                {
+                    Sheet = workbook.CreateSheet(SheetName[AllSheet]);
+                }
+                else
+                {
+                    Sheet = workbook.CreateSheet("Sheet" + AllSheet);
+                }
+                Sheet.CreateRow(0);
+                for (int i = 0; i < Source[AllSheet].Columns.Count; i++)
+                {
+                    Sheet.GetRow(0).CreateCell(i).SetCellValue(Source[AllSheet].Columns[i].ColumnName);
+                    HSSFCell cell = (HSSFCell)Sheet.GetRow(0).GetCell(i);
+                    cell.CellStyle = csTitle;
+                }
+                for (int i = 0; i < Source[AllSheet].Rows.Count; i++)
+                {
+                    Sheet.CreateRow(i + 1);
+                    for (int x = 0; x < Source[AllSheet].Columns.Count; x++)
+                    {
+                        Sheet.GetRow(i + 1).CreateCell(x).SetCellValue(Source[AllSheet].Rows[i][x].ToString());
+                        HSSFCell cell = (HSSFCell)Sheet.GetRow(i + 1).GetCell(x);
+                        cell.CellStyle = csCell;
+                    }
+                }
+                for (int i = 0; i < Source[AllSheet].Columns.Count; i++)
+                {
+                    Sheet.AutoSizeColumn(i);
+                }
+            }
+            FileStream SaveFile = new FileStream(SavePath, FileMode.Create);
+            try
+            {
+                workbook.Write(SaveFile);
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                writerLog.WriteLine(DateTime.Now.ToString(datetimeFormat) + "  " + ex.Message + "\r\n");
+                writerLog.Flush();
+            }
+            SaveFile.Close();
+            return result;
+        }
+
+        private void SendMail(string from,string display,string to,string sub,string body,string att)
         {
             //建立寄件者地址與名稱
-            MailAddress ReceiverAddress = new MailAddress("sm4@ewpcb.com.tw", "每日預估入庫SPnl數量統計");
+            MailAddress ReceiverAddress = new MailAddress(from,display);
             //建立收件者地址
-            MailAddress SendAddress = new MailAddress("spnlcount@ewpcb.com.tw");
-            //建立附加檔案
-            Attachment attachment = new Attachment(SaveFile);
+            MailAddress SendAddress = new MailAddress(to);
             //建立E-MAIL相關設定與訊息
             MailMessage SendMail = new MailMessage(ReceiverAddress, SendAddress);
             //Mail以HTML格式寄送
@@ -114,10 +228,14 @@ namespace EverydaySPnlCountService
             SendMail.SubjectEncoding = Encoding.UTF8;
             //設定信件優先權為普通
             SendMail.Priority = MailPriority.Normal;
-            SendMail.Subject = DateTime.Now.ToString("yyyy-MM-dd") + " 每日預估入庫SPnl數量統計！";//主旨
-            SendMail.Body = "每日預估入庫SPnl數量統計，請詳閱附件。" + "<br/>" + "<br/>" +
-                "-----此封郵件由系統所寄出，請勿直接回覆！-----";//內容
-            SendMail.Attachments.Add(attachment);//加上附件檔案
+            SendMail.Subject = sub;//主旨
+            SendMail.Body = body;//內容
+            if (att != "")
+            {
+                //建立附加檔案
+                Attachment attachment = new Attachment(att);
+                SendMail.Attachments.Add(attachment);//加上附件檔案
+            }
             //建立一個信件通訊並設定郵件主機地址與通訊埠號
             SmtpClient MySmtp = new SmtpClient("ms1.ewpcb.com.tw", 25);
             //設定寄件者的帳號與密碼
