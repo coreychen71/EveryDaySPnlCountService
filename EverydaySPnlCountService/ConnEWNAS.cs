4adaa953e40028240ec96864f5d0b32ba04d8c64
coreychen71 <coreychen71@gmail.com>
2017-02-13 08:21:53 +0800

    2017/2/13 1.每日客訴未逾期新增處理結果欄位

--- EverydaySPnlCountService/ConnEWNAS.cs
+++ EverydaySPnlCountService/ConnEWNAS.cs
@@ -20,9 +20,9 @@ namespace EverydaySPnlCountService
             var result = new DataTable();
             var strComm = "select custermerid '客戶別',CONVERT(char(10),cpldate,120) '客訴日期',datecode '週期'," +
                 "PARTNUM '料號',custermerpartnum '客戶料號',QNTY '數量',defectname '原因',discribe '描述'," +
-                "dutydp '責任單位',dutyman '責任人',CONVERT(char(10), requirdate, 120) '預計完成日',writeman '登記人員'," +
-                "CONVERT(char(10), writedate, 120) '登記日期' from qacustermercpl where stateflag is null " +
-                "order by cpldate,custermerid,PARTNUM";
+                "result '處理結果',dutydp '責任單位',dutyman '責任人',CONVERT(char(10), requirdate, 120) '預計完成日'," +
+                "writeman '登記人員',CONVERT(char(10), writedate, 120) '登記日期' from qacustermercpl " +
+                "where stateflag is null order by cpldate desc,custermerid,PARTNUM";
             using (SqlConnection sqlcon = new SqlConnection(strCon))
             {
                 SqlCommand sqlcomm = new SqlCommand(strComm, sqlcon);
